fd589da0ff2eaf9aad613667e5ac08ce94352894
lsivitz <lumen.sivitz@gmail.com>
2020-07-02 11:07:13 -0700

    flask backend

---
+++ api/api.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Video(Resource):
+    def get(self):
+        return {'time': time.time()}
+
+api.add_resource(Video, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
